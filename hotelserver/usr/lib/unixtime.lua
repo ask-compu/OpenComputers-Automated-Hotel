293af489fdd439ec3636740212873e5396d88710
Compu <hotwheels14901@me.com>
2022-06-24 10:10:39 -0400

    add requirement comment

--- hotelserver/usr/lib/unixtime.lua
+++ hotelserver/usr/lib/unixtime.lua
@@ -1,6 +1,8 @@
 local internet = require("internet")
 local json = require("json")
 
+--Requires http://github.com/craigmj/json4lua/
+
 local function time()
   jsonraw = tostring(internet.request("http://worldtimeapi.org/api/timezone/Etc/UTC")())
   decoded = json.decode(jsonraw)
