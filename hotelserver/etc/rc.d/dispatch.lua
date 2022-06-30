5d9e7f92515adb041a2549e9a53ad489199e315a
Compu <hotwheels14901@me.com>
2022-06-30 13:34:49 -0400

    adding a command queue
    Signed-off-by: Compu <hotwheels14901@me.com>

--- hotelserver/etc/rc.d/dispatch.lua
+++ hotelserver/etc/rc.d/dispatch.lua
@@ -3,6 +3,7 @@ local event = require("event")
 local modem = component.modem
 local cereal = require("serialization")
 local fs = require("filesystem")
+local queue = {}
 
 local bellhopaddress = "/etc/bellhopaddress.cfg"
 local currentjobfile = "/etc/currentjob.cfg"
@@ -16,22 +17,32 @@ local function loadaddr()
   return bellhop
 end
 
-local function currentjobwrite(command,room,username)
+local function currentjobwrite(command,room,username,remove)
   local file = io.open(currentjobfile,"w")
-  local srl = cereal.serialize({command=command,room=room,username=username})
+  local job = {command=command,room=room,username=username}
+  if remove then
+    table.remove(queue, 1)
+  else
+    table.insert(queue,job)
+  end
+  local srl = cereal.serialize(queue)
   file:write(srl)
   file:close()
   return
 end
 
 local function currentjobread()
-  local currentjob = {}
   if fs.exists(currentjobfile) then
     local file = io.open(currentjobfile)
     local srl = file:read("*a")
     file:close()
-    currentjob = cereal.unserialize(srl)
-    return currentjob["command"],currentjob["room"],currentjob["username"]
+    local queue = cereal.unserialize(srl)
+    local currentjob = queue[1]
+    if currentjob then
+      return currentjob["command"],currentjob["room"],currentjob["username"]
+    else
+      return nil, nil, nil
+    end
   end
   return nil, nil, nil
 end
@@ -49,7 +60,7 @@ local function handleDispatch(_,command,room,username)
       modem.send(loadaddr(),1212,command,room,username)
     end
   elseif command == "jobcomplete" then
-    currentjobwrite(nil,nil,nil)
+    currentjobwrite(nil,nil,nil,true)
   end
 end
 
